0f7492c7dc214ee7382ce42a44832cc5e852de86
Sander van 't Einde <github@sandervanteinde.nl>
2024-08-28 20:22:50 +0200

    Fix template

--- src/SourceGenTemplates.Sample/Tester.cs
+++ src/SourceGenTemplates.Sample/Tester.cs
@@ -5,9 +5,9 @@ namespace SourceGenTemplates.Sample;
 [GenerateBuilder]
 public partial class Tester
 {
-    private ClassInDifferentNamespace _class;
+    private ClassInDifferentNamespace? _class;
     private int _integer;
-    private string _string;
+    private string? _string;
 
     public void Print()
     {
